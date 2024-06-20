8586247931cd10ce3b3c3af2393073bf49d09995
Suneth Udayanga <sunethuw@gmail.com>
2024-06-20 09:44:42 +0530

    main config

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text className='text-xl text-red-700'>index</Text>
+    </View>
+  )
+}
+
+export default index
