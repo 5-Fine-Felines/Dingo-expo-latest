27b419799b48a871f466c45fe7e821c7548e559e
Suneth Udayanga <sunethuw@gmail.com>
2024-06-25 01:27:28 +0530

    supa connect

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
+import { supabase } from './lib/supabase';
 
 const index = () => {
+  const [clist, setclist] = useState([]);
+
+  useEffect(() => {
+    const fetchList = async () => {
+      console.log('fetching.....');
+
+      let { data: user, error } = await supabase
+        .from('user')
+        .select('*');
+        console.log(user);
+
+    };
+    fetchList();
+  });
+
   return (
     <View className='flex-1 m-auto justify-center'>
       <Text className='text-xl text-red-700'>index</Text>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => {
+
+      }}>
         <View className='w-32 bg-blue-300 p-2 justify-center m-auto rounded'>
           <Text className='m-auto'>Click</Text>
         </View>
