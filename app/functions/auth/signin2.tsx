6a08b7b8c7328950e92a08b8d266839950fe321d
Suneth Udayanga <sunethuw@gmail.com>
2024-07-01 02:45:50 +0530

    Update signin2.tsx

--- app/functions/auth/signin2.tsx
+++ app/functions/auth/signin2.tsx
@@ -35,16 +35,5 @@ export default async function onGoogleButtonPress() {
     return { user };
   }
 
-// export default async function onGoogleButtonPress() {
-//     // Check if your device supports Google Play
-//     await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-//     // Get the users ID token
-//     const { idToken } = await GoogleSignin.signIn();
 
-//     // Create a Google credential with the token
-//     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-//     // Sign-in the user with the credential
-//     return auth().signInWithCredential(googleCredential);
-// }
 
