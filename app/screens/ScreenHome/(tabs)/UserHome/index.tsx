91a69a4d6bd3df955323708c5580125895db8963
Charitha Bandara <charithac.cb@gmail.com>
2024-07-02 13:06:24 +0530

    searchbar and headerbar components

--- app/screens/ScreenHome/(tabs)/UserHome/index.tsx
+++ app/screens/ScreenHome/(tabs)/UserHome/index.tsx
@@ -1,16 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
 import { Stack } from 'expo-router'
+import SearchBar from '@/app/screens/ScreenHome/components/SearchBar';
 import HeaderBar from '@/app/screens/ScreenHome/components/HeaderBar';
 
 const index = () => {
-  return (
-    <View>
-      <HeaderBar />
-      <Stack.Screen options={{ headerShown:false}} />
-      <Text>index</Text>
-    </View>
-  )
-}
+  const [searchText, setSearchText] = useState<string>('');
 
-export default index
+  const handleSearch = () => {
+      console.log('Searching for:', searchText);
+  };
+
+  return (
+      <View style={styles.container}>
+        <HeaderBar/>
+          <SearchBar 
+              searchText={searchText} 
+              setSearchText={setSearchText} 
+              handleSearch={handleSearch} 
+          />
+          {/* Other components or content for UserHome */}
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      padding: 16,
+      backgroundColor: '#f5f5f5',
+  },
+});
+
+export default index;
