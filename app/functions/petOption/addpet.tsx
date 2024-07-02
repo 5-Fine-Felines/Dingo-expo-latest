f8a9e86135f93fbe89e7a94b8edfae17e6f65f88
Suneth Udayanga <sunethuw@gmail.com>
2024-07-02 18:19:28 +0530

    add pet

---
+++ app/functions/petOption/addpet.tsx
@@ -0,0 +1,29 @@
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import { supabase } from '@/app/lib/supabase'
+
+type PetData = {
+    pname: string;
+    pbreed: string | null;
+    psex: string;
+    ptype: string | null;
+    page:string | null;
+    pnote:string | null;
+
+  };
+
+const addpet = async (petData:PetData) => {
+    console.log(petData);
+  
+const { data, error } = await supabase
+.from('pet')
+.insert([
+  { name:petData.pname, breed:petData.pbreed, sex:petData.psex, species:petData.ptype, age:petData.page, pnote:petData.pnote },
+])
+.select()
+        
+}
+
+export default addpet
+          
