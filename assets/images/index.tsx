1ac220e5652ff629e4ebf37c44df56bb0d7dfe02
Suneth Udayanga <sunethuw@gmail.com>
2024-07-02 11:13:08 +0530

    update

--- assets/images/index.tsx
+++ assets/images/index.tsx
@@ -1,6 +1,10 @@
 const IMAGES = {
     SPLASHPET: require('./amusedtallcirripedmax1mb-1.png'),
     GOOGLEICON: require('./google-1.png'),
+    NAVHOME: require('./home.png'),
+    NAVFEED: require('./playlist.png'),
+    NAVSEARCH: require('./search2.png'),
+    NAVLIBRARY: require('./books.png'),
 
 }
 
