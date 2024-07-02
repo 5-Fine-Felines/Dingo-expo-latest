e09b2e5774350afd17bf386740c7b5f5f56e6003
Suneth Udayanga <sunethuw@gmail.com>
2024-07-02 13:55:40 +0530

    set header

--- app/screens/ScreenHome/(tabs)/UserHome/index.tsx
+++ app/screens/ScreenHome/(tabs)/UserHome/index.tsx
@@ -1,12 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
-import HeaderBar from '@/app/screens/ScreenHome/components/HeaderBar';
+
 
 const index = () => {
   return (
     <View>
-      <HeaderBar />
       <Stack.Screen options={{ headerShown:false}} />
       <Text>index</Text>
     </View>
